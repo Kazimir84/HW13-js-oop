ba36a1709d73b7676d8a17579c500be5f9911b83
Kazimir84 <kav2007@ukr.net>
2021-08-07 14:10:57 +0300

    Add 3 files with Homework

---
+++ main.js
@@ -0,0 +1,85 @@
+let fullFormat = {                             
+    hidden: function() {
+        seconds.remove.className = 'hidden';
+    },
+    shortFormat: function() {
+        clockTime.remove.className = 'shortFormat';
+    },
+    fullFormat: function() {
+        clockTime.className = 'fullFormat';
+    },             
+};
+
+let shortFormat = {       
+    hidden: function() {
+        seconds.className = 'hidden';
+    },  
+    fullFormat: function() {
+        clockTime.remove.className = 'fullFormat';
+    },
+    shortFormat: function() {
+        clockTime.className = 'shortFormat';
+    },           
+};
+
+class Time {
+    constructor() {
+        this.render = function() {
+            let clockTime = document.getElementById('clockTime');
+            let time = new Date();
+            let hours = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
+            let minuts = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+            let seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+    
+            clockTime.children[0].innerHTML = hours + ' ' + ':';
+            clockTime.children[1].innerHTML = minuts;
+            clockTime.children[2].innerHTML = ':' + ' ' + seconds;
+    
+            setInterval(this.render.bind(this), 200);     
+        };
+
+        clockTime.onclick = function toggle() {
+    
+            if(clockTime.className === 'shortFormat') {
+                seconds.classList.toggle('hidden');
+                clockTime.remove.className = 'shortFormat';
+                clockTime.className = 'fullFormat';
+                console.log('Отображается полный вариант времени, с секундами!'); 
+                alert('Отображается полный вариант времени, с секундами!');
+
+            } else if (clockTime.className === 'fullFormat') {
+                seconds.classList.toggle('hidden');
+                clockTime.remove.className = 'fullFormat';
+                clockTime.className = 'shortFormat';
+                console.log('Отображается короткий вариант времени, без секунд!'); 
+                alert('Отображается короткий вариант времени, без секунд!');
+            };
+        };
+    
+        this.formats = function(format) {
+    
+            if (format === fullFormat) {
+               fullFormat.hidden();
+               fullFormat.shortFormat();
+               fullFormat.fullFormat();
+               console.log('Отображается полный вариант времени, с секундами!'); 
+               alert('Отображается полный вариант времени, с секундами!');
+            
+            } else if (format === shortFormat) {
+               shortFormat.hidden();
+               shortFormat.shortFormat();
+               shortFormat.fullFormat();
+               console.log('Отображается короткий вариант времени, без секунд!'); 
+               alert('Отображается короткий вариант времени, без секунд!');
+            };
+        };
+    };
+};
+
+let clock = new Time();
+clock.render();
+
+// Вариант отображения часов при запуске!
+
+// clock.formats(shortFormat);
+clock.formats(fullFormat);
