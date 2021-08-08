ff3a98a5ba4c0c6ab2973e471b965bfedef0b4b4
Kazimir84 <72868574+Kazimir84@users.noreply.github.com>
2021-08-08 20:23:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -99,98 +99,99 @@ clock.formats(clock.fullFormat);
 
 // Второй вариант решения.
 // Вариант отображения часов при запуске! Через аргументы переданные в конструктор.
-class Clock {
-  constructor({ template }) {
 
-    this.template = template; 
+// class Clock {
+//   constructor({ template }) {
 
-    if (this.template === 'fullFormat') {
-      clockTime.className = 'fullFormat';
-      this.fullFormat();
+//     this.template = template; 
 
-    } else if (this.template === 'shortFormat') {
-      clockTime.className = 'shortFormat';
-      this.shortFormat();
+//     if (this.template === 'fullFormat') {
+//       clockTime.className = 'fullFormat';
+//       this.fullFormat();
 
-    } else {
-      console.log('Введите один из вариантов отображения fullFormat или shortFormat');
-      alert('Напишите или выберите один из вариантов отображения fullFormat или shortFormat');      
-    }
+//     } else if (this.template === 'shortFormat') {
+//       clockTime.className = 'shortFormat';
+//       this.shortFormat();
 
-    clockTime.onclick = function toggl() {
+//     } else {
+//       console.log('Введите один из вариантов отображения fullFormat или shortFormat');
+//       alert('Напишите или выберите один из вариантов отображения fullFormat или shortFormat');      
+//     }
+
+//     clockTime.onclick = function toggl() {
   
-      if (clockTime.className === 'shortFormat') {
-          seconds.classList.toggle('hidden');
-          clockTime.remove.className = 'shortFormat';
-          clockTime.className = 'fullFormat';          
-          clockTime.children[2].innerHTML = ":" + this.seconds;
-          console.log('Отображается полный вариант времени, с секундами!'); 
-          alert('Отображается полный вариант времени, с секундами!');
+//       if (clockTime.className === 'shortFormat') {
+//           seconds.classList.toggle('hidden');
+//           clockTime.remove.className = 'shortFormat';
+//           clockTime.className = 'fullFormat';          
+//           clockTime.children[2].innerHTML = ":" + this.seconds;
+//           console.log('Отображается полный вариант времени, с секундами!'); 
+//           alert('Отображается полный вариант времени, с секундами!');
 
-        } else if (clockTime.className === 'fullFormat') {
-          seconds.classList.toggle('hidden');
-          clockTime.remove.className = 'fullFormat';
-          clockTime.className = 'shortFormat';          
-          clockTime.children[2].className = 'hidden';
-          console.log('Отображается короткий вариант времени, без секунд!'); 
-          alert('Отображается короткий вариант времени, без секунд!');
-        };
-    };
-  };
+//         } else if (clockTime.className === 'fullFormat') {
+//           seconds.classList.toggle('hidden');
+//           clockTime.remove.className = 'fullFormat';
+//           clockTime.className = 'shortFormat';          
+//           clockTime.children[2].className = 'hidden';
+//           console.log('Отображается короткий вариант времени, без секунд!'); 
+//           alert('Отображается короткий вариант времени, без секунд!');
+//         };
+//     };
+//   };
 
-  render() {
-    let time = new Date();
-    let clockTime = document.getElementById('clockTime');
+//   render() {
+//     let time = new Date();
+//     let clockTime = document.getElementById('clockTime');
 
-    let hours = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
-    let minuts = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
-    let seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+//     let hours = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
+//     let minuts = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+//     let seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
 
-    if (this.template === 'shortFormat') {  
-      clockTime.children[0].innerHTML = hours + ":"; 
-      clockTime.children[1].innerHTML = minuts;      
-      clockTime.children[2].innerHTML = ":" + seconds;   
+//     if (this.template === 'shortFormat') {  
+//       clockTime.children[0].innerHTML = hours + ":"; 
+//       clockTime.children[1].innerHTML = minuts;      
+//       clockTime.children[2].innerHTML = ":" + seconds;   
       
-    } else if (this.template === 'fullFormat') {
-      clockTime.children[0].innerHTML = hours + ":"; 
-      clockTime.children[1].innerHTML = minuts;
-      clockTime.children[2].innerHTML = ":" + seconds;      
-    }   
-  }  
+//     } else if (this.template === 'fullFormat') {
+//       clockTime.children[0].innerHTML = hours + ":"; 
+//       clockTime.children[1].innerHTML = minuts;
+//       clockTime.children[2].innerHTML = ":" + seconds;      
+//     }   
+//   }  
 
-  fullFormat = function() {
-    this.template ='fullFormat';
-    console.log('Установлен полный вариант загрузки времени, с секундами!');
-    alert('Установлен полный вариант загрузки времени, с секундами!');
-  }
+//   fullFormat = function() {
+//     this.template ='fullFormat';
+//     console.log('Установлен полный вариант загрузки времени, с секундами!');
+//     alert('Установлен полный вариант загрузки времени, с секундами!');
+//   }
 
-  shortFormat = function() {
-    this.template = 'shortFormat';      
-    clockTime.children[2].className = 'hidden';
-    clockTime.className = 'shortFormat';    
-    console.log('Установлен короткий вариант загрузки времени, без секунд!'); 
-    alert('Установлен короткий вариант загрузки времени, без секунд!');
-  } 
-}
+//   shortFormat = function() {
+//     this.template = 'shortFormat';      
+//     clockTime.children[2].className = 'hidden';
+//     clockTime.className = 'shortFormat';    
+//     console.log('Установлен короткий вариант загрузки времени, без секунд!'); 
+//     alert('Установлен короткий вариант загрузки времени, без секунд!');
+//   } 
+// }
 
-// Вариант отображения часов при запуске! Через аргумент конструктора.
+// // Вариант отображения часов при запуске! Через аргумент конструктора.
 
-// Вариант отображения часов при запуске! Через аргумент конструктора, без секунд!
-// let clock = new Clock({template: 'shortFormat'});
+// // Вариант отображения часов при запуске! Через аргумент конструктора, без секунд!
+// // let clock = new Clock({template: 'shortFormat'});
 
-// Вариант отображения часов при запуске! Через аргумент конструктора, с секундами!
-let clock = new Clock({template: 'fullFormat'});
+// // Вариант отображения часов при запуске! Через аргумент конструктора, с секундами!
+// let clock = new Clock({template: 'fullFormat'});
 
-// Вариант отображения часов при запуске! Через аргумент конструктора, дефолтный!
-// let clock = new Clock({template: 'fullForma'});
+// // Вариант отображения часов при запуске! Через аргумент конструктора, дефолтный!
+// // let clock = new Clock({template: 'fullForma'});
 
-clock.render();
-setInterval(() => clock.render(), 200);
+// clock.render();
+// setInterval(() => clock.render(), 200);
 
-// Вариант отображения часов при запуске!
+// // Вариант отображения часов при запуске!
 
-    //Отображение с секундами
-// clock.fullFormat();
+//     //Отображение с секундами
+// // clock.fullFormat();
 
-    //Отображение без секунд
-// clock.shortFormat();
+//     //Отображение без секунд
+// // clock.shortFormat();
